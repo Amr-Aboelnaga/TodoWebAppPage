7da31b802ec57769ab94b0d0f05e30d6109af60f
AmrAhmedd <amr.aboelnaga@student.guc.edu.eg>
2020-09-14 07:46:15 +0200

    Updates

---
+++ precache-manifest.266091840ce6add3f30f007b5cd03f2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50fa386e29f7f784561db5f83601fe01",
+    "url": "/todoWebAppPage/index.html"
+  },
+  {
+    "revision": "ea50cddd692e2c832f1f",
+    "url": "/todoWebAppPage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/todoWebAppPage/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoWebAppPage/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea50cddd692e2c832f1f",
+    "url": "/todoWebAppPage/static/js/main.a177921b.chunk.js"
+  },
+  {
+    "revision": "805617a1b1a0fa7d0192",
+    "url": "/todoWebAppPage/static/js/runtime-main.20546a34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoWebAppPage/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TodoWebAppPage/precache-manifest.077b62ff88757f083574564595ccf41e.js"
+  "/todoWebAppPage/precache-manifest.266091840ce6add3f30f007b5cd03f2c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TodoWebAppPage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todoWebAppPage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
