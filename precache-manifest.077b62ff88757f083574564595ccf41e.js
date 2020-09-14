8cb45b5b6d664b5894903d769f40dc6f511f8f59
AmrAhmedd <amr.aboelnaga@student.guc.edu.eg>
2020-09-14 07:34:41 +0200

    Updates

---
+++ precache-manifest.077b62ff88757f083574564595ccf41e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1bef043e1bd0dee8d4821842b51cdb",
+    "url": "/TodoWebAppPage/index.html"
+  },
+  {
+    "revision": "5429669ddb1a3bcf2e53",
+    "url": "/TodoWebAppPage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/TodoWebAppPage/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoWebAppPage/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5429669ddb1a3bcf2e53",
+    "url": "/TodoWebAppPage/static/js/main.2dbf4cce.chunk.js"
+  },
+  {
+    "revision": "e35c4622891aaa991e5c",
+    "url": "/TodoWebAppPage/static/js/runtime-main.bccd22c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoWebAppPage/static/media/logo.5d5d9eef.svg"
+  }
+]);
